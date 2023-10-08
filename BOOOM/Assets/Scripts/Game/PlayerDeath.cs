2dec5985c5526c1ecd57a0cc8e3376501e5754a1
HuaiMengXing <1549283498@qq.com>
2023-10-08 22:29:17 +0800

    add

---
+++ BOOOM/Assets/Scripts/Game/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public Transform eatPos;
+    public float eatSpeed;
+    public TextAsset text;
+
+    private bool one = true;
+    void Update()
+    {
+        if(Player.Instance.death)
+        {
+            if(text != null && one)
+            {
+                one = false;
+                DialogSystem.Instance.GetTextFromFile(text);
+                DialogSystem.Instance.gameObject.SetActive(true);
+            }
+            transform.position = Vector3.Lerp(transform.position, eatPos.position, eatSpeed * Time.deltaTime);   
+        }
+    }
+}
